160472787b26b83706ca6568053ee6cb0b38c724
Samzee712 <85202514+Samzee712@users.noreply.github.com>
2025-02-13 13:56:51 +0100

    Delete projects directory

--- projects/Scripts/siteplan.js
+++
@@ -1,3 +0,0 @@
- // Dynamic Year & Last Modified
- document.getElementById("currentYear").textContent = new Date().getFullYear();
- document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
